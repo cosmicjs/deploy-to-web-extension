bf0ad3f3a2db1f2f2fd2e8095377e973b8da3555
Tony Spiro <tspiro@tonyspiro.com>
2019-08-23 15:25:22 -0500

    EDIT: icon and title

---
+++ build/precache-manifest.e23e79464b4853f38b7e1ecc1ef9ddc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fdc76e187bc156037f9b9aec24bb90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8dbf44ee88fe07daa5a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3933b2282c7157272571",
+    "url": "/static/css/main.8926c892.chunk.css"
+  },
+  {
+    "revision": "a8dbf44ee88fe07daa5a",
+    "url": "/static/js/2.160a052f.chunk.js"
+  },
+  {
+    "revision": "3933b2282c7157272571",
+    "url": "/static/js/main.755a3f8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
